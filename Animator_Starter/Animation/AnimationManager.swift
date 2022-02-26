2f9c66c09371ff39b064f085bf864eba91478410
Yessenali Zhanaidar <yessenalijr@gmail.com>
2022-02-26 15:24:16 +0600

    Exercises

---
+++ Animator_Starter/Animation/AnimationManager.swift
@@ -0,0 +1,35 @@
+//
+//  AnimationManager.swift
+//  Animator_Starter
+//
+//  Created by Yessenali Zhanaidar on 26.02.2022.
+//  Copyright © 2022 Paradigm Shift Development, LLC. All rights reserved.
+//
+
+import UIKit
+
+class AnimationManager {
+    
+    // Calculate screen bounds
+    class var screenBounds: CGRect {
+        return UIScreen.main.bounds
+    }
+    
+    // Screen Positions
+    
+    class var screenRight: CGPoint {
+        return CGPoint(x: screenBounds.maxX, y: screenBounds.midY)
+    }
+    
+    class var screenTop: CGPoint {
+        return CGPoint(x: screenBounds.midX, y: screenBounds.minY)
+    }
+    
+    class var screenLeft: CGPoint {
+        return CGPoint(x: screenBounds.minX, y: screenBounds.midY)
+    }
+    
+    class var screenBottom: CGPoint {
+        return CGPoint(x: screenBounds.midX, y: screenBounds.maxY)
+    }
+}
